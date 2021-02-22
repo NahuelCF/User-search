c8b2413052c0a92129ab007e46d1c98aca23c80c
Nahuel <cristiannahuel95@gmail.com>
2021-02-22 17:11:03 -0300

    changing User class

--- user-search/src/Domain/user.js
+++ user-search/src/Domain/user.js
@@ -1,14 +1,18 @@
 export class User {
 
-    constructor(username, id) {
+    constructor(username, id, avatar_url, profile_url) {
         this.username = username
         this.id = id
+        this.avatar_url = avatar_url
+        this.profile_url = profile_url
     }
 
     static fromJSON(userJSON) {
         return new User(
             userJSON.login || userJSON.username,
-            userJSON.id
+            userJSON.id,
+            userJSON.avatar_url,
+            userJSON.url || userJSON.web_url
         )
     }
 }
