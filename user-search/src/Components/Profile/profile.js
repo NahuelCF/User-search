d844ed49adbb790698375523521815b03c587895
Nahuel <cristiannahuel95@gmail.com>
2021-02-22 17:14:11 -0300

    now clicking the card redirect to the userProfile view and show info about the user

--- user-search/src/Components/Profile/profile.js
+++ user-search/src/Components/Profile/profile.js
@@ -1,5 +1,30 @@
+import { React, useContext, useEffect, useState } from 'react'
+import { User } from "../../Domain/user"
+import { getUserFromGitHub, getUserFromGitLab } from '../../service'
+
 export const Profile = (props) => {
+    const username = props.location.state.username
+    const api = props.location.state.api
+    const [user, setUser] = useState(new User())
+
+    useEffect(() => {
+        const getUser = async () => {
+            let user
+            if (api == "github") {
+                user = await getUserFromGitHub(username)
+            } else {
+                user = await getUserFromGitLab(username)
+                console.log(user)
+            }
+            setUser(user)
+        }
+        getUser()
+    }, [])
+
     return (
-        <div>UserProfile Test</div>
+        <div>
+            UserProfile {user.id}
+            <img src={user.avatar_url}></img>
+        </div>
     )
 }
--- user-search/src/Components/UserCard/usercard.js
+++ user-search/src/Components/UserCard/usercard.js
@@ -2,9 +2,17 @@ import "./usercard.css"
 
 export const UserCard = (props) => {
     const user = props.user
+    const api = props.api
+
+    const goToProfile = () => {
+        props.history.push({
+            pathname: "/userProfile",
+            state: { username: user.username, api: props.api }
+        })
+    }
 
     return (
-        <div className="usercard">
+        <div className="usercard" onClick={goToProfile}>
             <div>Username: {user.username}</div>
             <div>ID: {user.id}</div>
         </div>
