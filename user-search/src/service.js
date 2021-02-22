d3388ace0438a890d4f19918b935f38f45fc98db
Nahuel <cristiannahuel95@gmail.com>
2021-02-22 17:11:37 -0300

    added functions to get an user

--- user-search/src/service.js
+++ user-search/src/service.js
@@ -10,6 +10,14 @@ export const getUsersFromGitHub = async (search) => {
     return users
 }
 
+export const getUserFromGitHub = async (username) => {
+    let user = new User()
+    await fetch("https://api.github.com/users/" + username)
+        .then(result => result.json())
+        .then(item => { user = User.fromJSON(item) })
+    return user
+}
+
 // Private Token: 9zXYGn_y4A5r9MP42xHx
 export const getUsersFromGitLab = async (search) => {
     // The only way that I find for search users that contains the search parameter is searching by name and showing 
@@ -24,4 +32,15 @@ export const getUsersFromGitLab = async (search) => {
             users = items.map(user => User.fromJSON(user)).slice(0, 12)
         })
     return users
+}
+
+export const getUserFromGitLab = async (username) => {
+    let user = new User()
+    await fetch("https://gitlab.com/api/v4/users?username=" + username, {
+        headers: {
+            "PRIVATE-TOKEN": "9zXYGn_y4A5r9MP42xHx"
+        }
+    }).then(result => result.json())
+        .then(items => { user = User.fromJSON(items[0]) })
+    return user
 }
