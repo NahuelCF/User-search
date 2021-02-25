04c32daec2177edce9d5659a10cf8929778001a8
Nahuel <cristiannahuel95@gmail.com>
2021-02-25 16:43:56 -0300

    added a template to show the icon of the API in profile

--- user-search/src/Components/Profile/profile.js
+++ user-search/src/Components/Profile/profile.js
@@ -16,12 +16,23 @@ export const Profile = (props) => {
                 user = await getUserFromGitHub(username)
             } else {
                 user = await getUserFromGitLab(username)
-                console.log(user)
             }
             setUser(user)
         }
         getUser()
     }, [])
+
+    const apiIconTemplate = () => {
+        if (api == "github") {
+            return (
+                <img style={{ height: "100px", width: "130px" }} src="https://github.githubassets.com/images/modules/logos_page/Octocat.png"></img>
+            )
+        } else {
+            return (
+                <img style={{ height: "100px", width: "100px" }} src="http://assets.stickpng.com/images/5847f997cef1014c0b5e48c1.png"></img>
+            )
+        }
+    }
 
     return (
         <div className="profile-container">
@@ -33,7 +44,9 @@ export const Profile = (props) => {
                 <h3>
                     ID: {user.id}
                 </h3>
-                <a href={user.profile_url} target="_blank">Go to Profile</a>
+                <a href={user.profile_url} target="_blank" title="Click to go to profile">
+                    {apiIconTemplate()}
+                </a>
             </div>
         </div>
     )
